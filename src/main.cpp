aacd67a7e14fa7e6475749fedcc5ab0fe2f74b50
Davide Molitierno <>
2022-12-04 20:07:05 +0100

    Introdotta libreria CGAL nel PerlinNoise

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
-#include "PerlinNoise.hpp"
+#include "../include/PerlinNoise/PerlinNoise.hpp"
 #include <fmt/format.h>
+#include <CGAL/Cartesian.h>
+#include <CGAL/Point_3.h>
+
+typedef CGAL::Cartesian<double> kernel;
+typedef CGAL::Point_3<kernel> point;
 
 int main()
 {
 	const siv::PerlinNoise::seed_type seed = 123456u;
 
 	const siv::PerlinNoise perlin{ seed };
+
+	int width = 100;
+	int height = 100;
+
+	point points[width * height];
 	
-	for (int y = 0; y < 100; ++y)
+	for (int y = 0; y < width; ++y)
 	{
-		for (int x = 0; x < 100; ++x)
+		for (int x = 0; x < height; ++x)
 		{
 			const double noise = perlin.octave2D_01((x * 0.01), (y * 0.01), 4);
-			
-			fmt::print("{}\t", noise);
+			points[(y + x) * y] = point(y, x, noise);
+			fmt::print("{}\t", points[(y + x) * y].z());
 		}
 
 		fmt::print("\n");
