bd7a75d1b69c61d96416411fd6dd43a125aac380
Davide Molitierno <>
2022-12-11 17:06:32 +0100

    Commit con modifiche al Perlin generator: ora è possibile specificare: -ampiezza -frequenza -ottave -seed -dimensioni della mappa Modifiche al progetto per la struttura

--- src/PerlinGenerator/PerlinGenerator.hpp
+++ src/PerlinGenerator/PerlinGenerator.hpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <CGAL/Point_3.h>
 #include <CGAL/Cartesian.h>
+#include "../../include/PerlinNoise/PerlinNoise.hpp"
 
 typedef CGAL::Cartesian<double> kernel;
 typedef CGAL::Point_3<kernel> point;
@@ -11,7 +12,7 @@ typedef CGAL::Point_3<kernel> point;
 class PerlinGenerator{
 public:
 	PerlinGenerator();
-	std::vector<point> GenerateTerrain(int width, int height, unsigned int seed);
+	std::vector<point> GenerateTerrain(int width, int height, unsigned int seed, int octaves, int amplitude, double frequency);
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
 #include <fmt/format.h>
+#include "../src/PerlinGenerator/PerlinGenerator.hpp"
 
-int main()
+int main(int argc, char const *argv[])
 {
-
+	PerlinGenerator pg;
+	fmt::print("Test\n");
+	return 0;
 }
