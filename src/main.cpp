2ecbf4282da460a956d1ece7cf1896d143726f91
Davide Molitierno <>
2022-12-11 17:56:14 +0100

    Modifica al gitignore per evitare il commit dei dati dentro data Introdotto script python plotpoints che permette di visualizzare quanto presente nella cartella data Creato un semplice programma in main che genera una matrice di punti col perlinNoise

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
 #include <fmt/format.h>
 #include "../src/PerlinGenerator/PerlinGenerator.hpp"
+#include <vector>
+#include <fstream>
 
 int main(int argc, char const *argv[])
 {
+	int width = 1000;
+	int height = 1000;
+
 	PerlinGenerator pg;
+	std::vector<point> points = pg.GenerateTerrain(width, height, 0u, 4, 10, 0.1);
+
+	std::ofstream out("../test/data/points.txt");
+	if(!out){
+		exit(1);
+	}
+	int counter = 0;
+	for(auto p: points){
+		if(counter >= width){
+			counter = 0;
+			out << "\n";
+		}
+		out << fmt::format("{} ",p.z());
+		counter++;
+	}
+	fmt::print("{}", points.size());
+	out.close();
 	fmt::print("Test\n");
 	return 0;
 }
