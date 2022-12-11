681e0eb16ddbcb0c94eb82275b0f6dacbd52cc2e
Davide Galletti <d.galletti99@gmail.com>
2022-12-12 00:00:34 +0100

    -

--- src/main.cpp
+++ src/main.cpp
@@ -7,27 +7,32 @@
 
 #include <fmt/format.h>
 
+#include <CGAL/IO/PLY/PLY_writer.h>
+#include <CGAL/IO/OBJ.h>
 
 int main(int argc, char const *argv[])
 {
 	int width = 100;
 	int height = 100;
 
-	tgen::PerlinGenerator pg;
-	auto points = pg.GenerateTerrain(width, height, 0u, 4, 10, 0.1);
 
-	const auto ts = std::chrono::duration_cast<std::chrono::seconds>(
+	const auto seed = std::chrono::duration_cast<std::chrono::seconds>(
 		std::chrono::system_clock::now().time_since_epoch()).count();
+
+	tgen::PerlinGenerator pg;
+	auto points = pg.GenerateTerrain(width, height, seed, 4, 10, 0.1);
 
 	tgen::Mesher mr;
 	tgen::Mesh m = mr.triangulate(points);
 
-	std::string filename = fmt::format("../data/{}.off", ts);
+	std::string filename = fmt::format("../data/{}.obj", seed);
 	
 	std::ofstream out(filename);
 	fmt::print("{} created\n", filename);
 
-	out << m;
+	CGAL::IO::write_OBJ(out, m);
+
+	// out << m;
 
 	return 0;
 }
