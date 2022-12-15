3454006c8f905d1c95962f9904770d58fd21360d
dedo99 <andreadedonato99@gmail.com>
2022-12-16 00:40:20 +0100

    Parziale implementazione OpenSimplex

---
+++ src/OpenSimplexGenerator/OpenSimplexGenerator.cpp
@@ -0,0 +1,10 @@
+/*#include "OpenSimplexGenerator.hpp"
+
+tgen::OpenSimplexGenerator::OpenSimplexGenerator(){}
+
+std::vector<tgen::Point> tgen::OpenSimplexGenerator::OpenSimplexGenerator(int width, int heigth, 
+	unsigned int seed, int octaves, int amplitude, double frequency) {
+
+	
+
+}*/
