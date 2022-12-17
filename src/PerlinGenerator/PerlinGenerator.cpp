90c180f1463d8e2bfe3efdb85e1fbe6337500a43
MatteoIng <matteofoschi10@gmail.com>
2022-12-17 11:44:37 +0100

    simplexNoise

--- src/PerlinGenerator/PerlinGenerator.cpp
+++ src/PerlinGenerator/PerlinGenerator.cpp
@@ -1,6 +1,7 @@
 #include "PerlinGenerator.hpp"
 #define STB_PERLIN_IMPLEMENTATION
 #include "./stb_perlin.h"
+#include <cmath>
 
 tgen::PerlinGenerator::PerlinGenerator(){}
 
@@ -12,7 +13,9 @@ std::vector<tgen::Point> tgen::PerlinGenerator::GenerateTerrain(int width, int h
 	std::vector<Point> points;
 	for( int x = 0 ; x < width; x++){
 		for ( int y = 0 ; y < heigth ; y++){
-			double noise = perlin.octave2D_01((x * frequency), (y * frequency), octaves) * amplitude;
+			//double noise = perlin.octave2D_01((x * frequency), (y * frequency), octaves) * amplitude;
+			double oct=std::pow(2,octaves);
+			double noise = stb_perlin_noise3((x * frequency), (y * frequency), 0,oct,oct,0) * amplitude;
 			points.push_back(Point(x, y, noise));
 		}
 	}
