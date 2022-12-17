70401664c24782ad853ca8983d54f65230078a79
Davide Molitierno <>
2022-12-17 13:03:26 +0100

    Introdotto polimorfismo iniziale nel PerlinGenerator

--- src/PerlinGenerator/PerlinGenerator.cpp
+++ src/PerlinGenerator/PerlinGenerator.cpp
@@ -6,12 +6,12 @@ tgen::PerlinGenerator::PerlinGenerator(){}
 std::vector<tgen::Point> tgen::PerlinGenerator::generateTerrain(int width, int heigth, 
 	unsigned int seed, int octaves, int amplitude, double frequency) {
 
-	tgen::PerlinNoise noise = tgen::PerlinNoise(seed, octaves);
+	tgen::Noise* noise = new tgen::PerlinNoise(seed, octaves);
 
 	std::vector<Point> points;
 	for( int x = 0 ; x < width; x++){
 		for ( int y = 0 ; y < heigth ; y++){
-			double elevation = noise.generateNoise((x * frequency), (y * frequency)) * amplitude;
+			double elevation = noise->generateNoise((x * frequency), (y * frequency)) * amplitude;
 			// fmt::print("{} {}\n", elevation, y);
 			points.push_back(Point(x, y, elevation));
 		}
