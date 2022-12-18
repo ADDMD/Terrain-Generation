d465532190c1387514d622956b872834e3599148
davidegattini <davidegatto99@gmail.com>
2022-12-18 21:08:13 +0100

    Scritto file per fbm e cancellati file inutilizzati

--- src/Noises/FBMNoise.cpp
+++ src/Noises/FBMNoise.cpp
@@ -1,11 +1,17 @@
 #include "Noise.hpp"
+#include "../../include/FastNoise/FastNoiseLite.h"
 //include dell fbm
 
 class tgen::FBMNoise: public tgen::Noise{
+	FastNoiseLite fbm;
 public:
-	FBMNoise(unsigned int seed) : Noise(seed){}
+	FBMNoise(unsigned int seed) : Noise(seed){
+		fbm.SetSeed(seed);
+		fbm.SetNoiseType(FastNoiseLite::NoiseType_Value);
+		fbm.SetFractalType(FastNoiseLite::FractalType_FBm);
+	}
 
 	double generateNoise(double x, double y, int octaves){
-		return 0;
+		return fbm.GetNoise(x, y);
 	}
 }; 
